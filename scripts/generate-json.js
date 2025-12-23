c12129820e0eda000cf1bd18c659238f5fb44b43
citruspuzzlemaker <citruspuzzlemaker@gmail.com>
2025-12-23 18:47:58 +0000

    Update generate-json.js

--- scripts/generate-json.js
+++ scripts/generate-json.js
@@ -5,7 +5,8 @@ const imagesDir = path.join(__dirname, "..", "images");
 const outputDir = path.join(__dirname, "..", "public");
 const outputFile = path.join(outputDir, "gallery.json");
 
-const baseUrl = "https://raw.githubusercontent.com/citruspuzzlemaker/puzzle-gallery/main/images";
+// Il tuo nuovo baseUrl per ImageKit
+const baseUrl = "https://ik.imagekit.io/cpuzzle";
 
 const files = fs.readdirSync(imagesDir).filter(f =>
   /\.(jpg|jpeg|png|gif|webp)$/i.test(f)
